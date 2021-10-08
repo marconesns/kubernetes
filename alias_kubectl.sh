89595445e29d0c7ee0b6e77f8c7f231e434cd62f
Marcones N. da Silva <marcones.silva@crediembrapa.local>
2021-10-08 15:05:17 -0300

    monitoring

--- alias_kubectl.sh
+++ alias_kubectl.sh
@@ -1,5 +1,22 @@
 alias 'k=kubectl'
 alias 'kl=kubectl logs -f'
+
+# Comandos GET
 alias 'kgp=kubectl get pods'
+alias 'kgd=kubectl get deployment'
+alias 'kgs=kubectl get service'
+alias 'kgn=kubectl get nodes'
+
+
+# Comando para Deletar
 alias 'kdp=kubectl delete pod'
 
+
+# Comandos Edit
+alias 'kep=kubectl edit pods'
+alias 'ked=kubectl edit deployment'
+
+# Comandos com Watch
+alias 'wkgp=watch -n1 kubectl get pods'
+alias 'wkgd=watch -n1 kubectl get deployment'
+alias 'wkgs=watch -n1 kubectl get service'
